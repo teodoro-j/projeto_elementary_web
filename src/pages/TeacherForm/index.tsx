e414b1f84c83e6f1b1c63eabdbc9305eac2f8724
teodorojo <joao.alfredoteodoro@gmail.com>
2020-08-09 18:04:52 -0300

    alterado css do schedule-item/input-block

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -9,63 +9,81 @@ import warningIcon from '../../assets/images/icons/warning.svg'
 import './styles.css';
 
 function TeacherForm() {
-    return(
+    return (
         <div id="page-teacher-form" className="container" >
-            
-           <PageHeader 
-           title="Quer dizer que você quer dar aulas." 
-           description="O primeiro passo é preencher esse formulário de inscrição"
-           color={'#006975'}
-           /> 
 
-           <main>
-               <fieldset>
-                    <legend>Seus dados</legend> 
+            <PageHeader
+                title="Quer dizer que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição"
+                color={'#006975'}
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
 
                     <Input name="name" label="Nome completo" />
-                    <Input name="avatar" label="Avatar"/>
-                    <Input name="whatsapp" label="WhatsApp"/>
-                    <Textarea name="bio"  label="Biografia" />
-               </fieldset>
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+                    <Textarea name="bio" label="Biografia" />
+                </fieldset>
 
-               <fieldset>
-                    <legend>Sobre a aula</legend> 
+                <fieldset>
+                    <legend>Sobre a aula</legend>
 
-                    <Select 
-                    name="subject" 
-                    label="Matéria"
-                    options={[
-                        { value: 'Banco de Dados', label: 'Banco de Dados'},
-                        { value: 'Back-end', label: 'Back-end'},
-                        { value: 'Engenharia de Software', label: 'Engenharia de Software'},
-                        { value: 'Estrutura de Dados', label: 'Estrutura de Dados'},
-                        { value: 'Front-end', label: 'Front-end'},
-                        { value: 'Redes', label: 'Redes'},
-                        { value: 'Sistemas Operacionais', label: 'Sistemas Operacionais'},
-                    ]}
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Banco de Dados', label: 'Banco de Dados' },
+                            { value: 'Back-end', label: 'Back-end' },
+                            { value: 'Engenharia de Software', label: 'Engenharia de Software' },
+                            { value: 'Estrutura de Dados', label: 'Estrutura de Dados' },
+                            { value: 'Front-end', label: 'Front-end' },
+                            { value: 'Redes', label: 'Redes' },
+                            { value: 'Sistemas Operacionais', label: 'Sistemas Operacionais' },
+                        ]}
                     />
-                    <Input name="cost" label="Custo da hora da sua aula"/>
-               </fieldset>
+                    <Input name="cost" label="Custo da hora da sua aula" />
+                </fieldset>
 
-               <fieldset>
-                   <legend>Horários disponíveis
+                <fieldset>
+                    <legend>Horários disponíveis
                    <button type="button">
-                       + Novo horário
+                            + Novo horário
                    </button>
-                   </legend>
-               </fieldset>
+                    </legend>
+                </fieldset>
 
-               <footer>
-                   <p>
-                       <img src={warningIcon} alt="Aviso Importante"/>
-                       Importante! < br/>
+                <div className="schedule-item">
+                    <Select
+                        name="week_day"
+                        label="Dia da Semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input name="from" label="Das" type="time"></Input>
+                    <Input name="to" label="Até" type="time"></Input>
+                </div>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importante" />
+                       Importante! < br />
                        Preencha todos os dados
                    </p>
-                   <button type="button"> 
-                       Salvar Cadastro
+                    <button type="button">
+                        Salvar Cadastro
                    </button>
-               </footer>
-           </main>
+                </footer>
+            </main>
         </div>
     )
 };
